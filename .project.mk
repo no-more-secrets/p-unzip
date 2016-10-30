bb22ab4843b56858fe90bdc70dc5f6b59012e7b8
David P. Sicilia <dpsicilia@gmail.com>
2016-10-30 10:12:45 -0400

    Some more make system changes

--- .project.mk
+++ .project.mk
@@ -1,16 +1,4 @@
-CFLAGS         += -MMD -MP -m64 -Wall -Wpedantic
-CXXFLAGS       += $(CFLAGS) -std=c++11 $(VISIBILITY_HIDDEN)
-
-CFLAGS_DEBUG   += $(CXXFLAGS) -g -ggdb
-CFLAGS_RELEASE += $(CXXFLAGS) -Ofast
-
-CFLAGS_LIB     += -fPIC
-
-ifdef OPT
-    CXXFLAGS_TO_USE = $(CFLAGS_RELEASE)
-else
-    CXXFLAGS_TO_USE = $(CFLAGS_DEBUG)
-endif
+CXXFLAGS += -std=c++11
 
 GCC_HOME=/usr/bin
 
@@ -18,13 +6,6 @@ CC  := $(GCC_HOME)/g++
 CXX := $(CC)
 LD  := $(CC)
 
-LDFLAGS     :=
-LDFLAGS_LIB := $(LDFLAGS) -shared 
-
 ENABLE_BIN_FOLDER = 1
 
-INSTALL_PREFIX := $(HOME)/tmp
-
-######################################
-CWD := $(TOPLEVELWD)
 $(call enter,src)
