e5f8b4f62cc9ad83db3745b73dd93a644d56d59f
David P. Sicilia <dpsicilia@gmail.com>
2017-02-12 13:37:14 -0500

    Add a precompiled header

---
+++ src/precomp.hpp
@@ -0,0 +1,15 @@
+// This is an optional pre-compiled header, generated
+// on 2017-02-12.  Seems to speed up a single-core
+// build by about 20% with gcc on os x.
+#include <algorithm>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <set>
+#include <stdexcept>
+#include <string>
+#include <thread>
+#include <vector>
+
+#include "macros.hpp"
+#include "utils.hpp"
