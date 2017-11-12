a34a7ef6ec063312539d325ef6780777bd267ecb
David P. Sicilia <unspecified>
2017-11-11 20:33:47 -0500

    Update makefiles for nr-make changes

--- .project.mk
+++ .project.mk
@@ -1,11 +1,8 @@
 CXXFLAGS += -std=c++11 -Wfatal-errors -pthread
 LDFLAGS  += -pthread
 
-GPLUSPLUS ?= /usr/bin/g++
-
-CC  = $(GPLUSPLUS)
-CXX = $(CC)
-LD  = $(CC)
+# Enable if you need to
+#STATIC_LIBSTDCXX=
 
 no_top_bin_folder = 1
 
@@ -13,11 +10,6 @@ ifeq ($(OS),OSX)
     TP_INCLUDES_EXTRA += -I/opt/local/include
     TP_LINK_EXTRA     += -L/opt/local/lib
     LIBZIP_INCLUDE    := -I/opt/local/lib/libzip/include
-    GCC_HOME           = /Users/dsicilia/dev/tools/bin
-else
-    # On linux let's do a static linkage
-    #LDFLAGS += -static -static-libgcc -static-libstdc++
-    LDFLAGS += -static-libgcc -static-libstdc++
 endif
 
 $(call enter,src)
